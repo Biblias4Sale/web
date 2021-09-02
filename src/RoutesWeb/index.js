b914800b55d0c94a20da5947bf43fe2a45736b67
Lina Gutierrez Arribas <mikaelagutierrezarribas@gmail.com>
2021-09-02 11:10:22 -0700

    Home

--- src/RoutesWeb/index.js
+++ src/RoutesWeb/index.js
@@ -1,14 +1,14 @@
 import { Route, Switch } from 'react-router-dom'
+import { Home } from '../components/Home/Home'
 // import { CreateAccount } from '../components/CreateAccount/CreateAccount'
-// import { Home } from '../components/Home/Home'
 // import { Login } from '../components/Login/Login'
 
 
 export const RouterWeb = () => {
   return (
     <Switch>
-      {/* <Route exact path='/' component={Home} />
-      <Route exact path='/login' component={Login} />
+       <Route exact path='/' component={Home} />
+      {/*<Route exact path='/login' component={Login} />
       <Route exact path='/create' component={CreateAccount} /> */}
     </Switch>
   )
---
+++ src/components/Home/HomeView.jsx
@@ -0,0 +1,57 @@
+import {
+  Container,
+  Col,
+  Row
+} from 'react-bootstrap'
+
+import LineaExpertos from '../../assets/lineaExpertos.jpg'
+import Accesorios from '../../assets/accesorios.jpg'
+import Camaras from '../../assets/camaras.jpg'
+import CargadoresYbaterias from '../../assets/cargadoresYbaterias.jpg'
+import Luces from '../../assets/luces.jpg'
+import Soportes from '../../assets/soportes.jpg'
+
+import { Link } from 'react-router-dom'
+
+// import { Style } from './HomeStyle'
+
+export const HomeView = () => {
+  return (
+    <div className='m-0'>
+      <Container>
+        <Row className='m-0'>
+          <Col className='m-0'>
+            <Link to=''>
+              <div><img className='w-100' alt='imagen no encontrada' src={LineaExpertos} /></div>
+            </Link>
+            <Link to=''>
+              <div><img className='w-100' alt='imagen no encontrada' src={Accesorios} /></div>
+            </Link>
+          </Col>
+          <Col>
+            <Link to=''>
+              <div><img className='w-100' height='900px' alt='imagen no encontrada' src={Camaras} /></div>
+            </Link>
+          </Col>
+        </Row>
+        <Row>
+          <Col>
+            <Link to=''>
+              <div><img className='w-100' alt='imagen no encontrada' src={Soportes} /></div>
+            </Link>
+          </Col>
+          <Col>
+            <Link to=''>
+              <div><img className='w-100' alt='imagen no encontrada' src={Luces} /></div>
+            </Link>
+          </Col>
+          <Col>
+            <Link to=''>
+              <div><img className='w-100' alt='imagen no encontrada' src={CargadoresYbaterias} /></div>
+            </Link>
+          </Col>
+        </Row>
+      </Container>
+    </div>
+  )
+}
