a475b538471fbeb9566097439e156bc04f662b69
erielilara <erieli.larad@gmail.com>
2021-09-03 07:16:35 -0500

    Paginado de Catálogo de productos

--- src/components/Products/Product.js
+++ src/components/Products/Product.js
@@ -2,13 +2,13 @@ import React from 'react';
 import './ProductStyle.css'
 
 
-export default function Product ({name, points}) {
+export default function Product ({name, points, id}) {
     return (
     
     <div class="col-xs-6 col-md-4">
        <div class="product tumbnail thumbnail-3"><a href="#"><img src="https://via.placeholder.com/350x280/87CEFA/000000" alt=""/></a>
          <div class="caption">
-             <h6><a href="#">{name}</a></h6>
+             <h6><a href="#">{name + '   ' + id}</a></h6>
              <span class="price sale">{points}</span>
          </div>
        </div>
