4474957e4fed99493511cce1435a1f62e04672d6
erielilara <erieli.larad@gmail.com>
2021-09-04 10:46:18 -0500

    menu de filtros creados

--- src/components/Catalogue/CatalogueStyle.js
+++ src/components/Catalogue/CatalogueStyle.js
@@ -1,4 +1,6 @@
-
+export const centerBanner = {
+  padding: '50px'
+}
 export const paginationStyle = {
   display: 'flex',
   justifyContent: 'space-between',
@@ -18,4 +20,21 @@ export const productsCat = {
   display: 'flex',
   flexWrap: 'wrap',
   justifyContent: 'space-around'
+}
+
+export const labelGoto = {
+  marginTop: '30px'
+}
+export const checkGoto = {
+  padding: '15px'
+}
+
+export const inputFilterby = {
+  display: 'flex',
+  justifyContent: 'space-between'
+}
+
+export const checkRating = {
+  padding: '5px',
+  display: 'flex'
 }
