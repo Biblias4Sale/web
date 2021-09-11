0138f03fcb8cf396093e90a0911b4b60e141e2f4
Ignacio Amatt <ignacio_amatt@hotmail.com>
2021-09-11 13:52:32 -0300

    Update Session.jsx

--- src/components/Session/Session.jsx
+++ src/components/Session/Session.jsx
@@ -5,7 +5,6 @@ import { useState } from 'react'
 
 export const Session = (props) => {
   const [showModal, setShowModal] = useState('init')
-  console.log('Estoy en session.js', showModal)
   return (
     <div>
       <Modal
@@ -17,7 +16,6 @@ export const Session = (props) => {
           .
         </Modal.Header>
         <Modal.Body>
-          {showModal !== 'init' && showModal !== 'create' ? <h1>Bienvenido {showModal} !</h1> : null}
           {showModal === 'init' ? <Login setShowModal={setShowModal} /> : null}
           {showModal === 'create' ? <CreateAccount setShowModal={setShowModal} /> : null}
           ACA VA EL BOTON GOOGLE
