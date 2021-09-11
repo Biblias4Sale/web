d4f270dbd1b5ecef4e89b255f569bb64bc3b5810
Ignacio Amatt <ignacio_amatt@hotmail.com>
2021-09-11 18:59:04 -0300

    Maneja el filtrado de categorias cuando veo resultados de busqueda

--- src/components/Catalogue/modules/viewComponent/rightContainer.js
+++ src/components/Catalogue/modules/viewComponent/rightContainer.js
@@ -8,21 +8,29 @@ export const RightContainer = ({ options, finalList, setOptions, clearCategory, 
   return (
     <Col lg={9}>
       <div className='d-flex mb-2'>
+
         {options.searching
           ? (
             <>
               <h3 style={{ marginRight: '0.5rem' }}>{options.searching}</h3>
-              <img src={btnBlue} alt='' height='30px' onClick={clearSearch} />
+              <img src={btnBlue} alt='' height='30px' onClick={clearSearch} style={{ marginRight: '1rem' }} />
               {/* <button type='button' class='btn-close' aria-label='Close' onClick={clearSearch} /> */}
+              {options.category
+                ? (
+                  <>
+                    <h5 style={{ marginRight: '0.5rem' }}>sólo en {options.category}</h5>
+                    <img src={btnBlue} alt='' height='30px' onClick={clearCategory} />
+                  </>
+                  )
+                : null}
             </>
             )
           : options.category
             ? (
               <>
                 <h3 style={{ marginRight: '0.5rem' }}>{options.category}</h3>
-                <img src={btnBlue} alt='' height='30px' onClick={clearSearch} />
+                <img src={btnBlue} alt='' height='30px' onClick={clearCategory} />
                 {/* <button type='button' class='btn-close' aria-label='Close' onClick={clearSearch} /> */}
-
               </>
               )
             : null}
