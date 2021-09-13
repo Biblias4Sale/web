e656a4abc4c97bda9fe781a55a47fad9d42cb038
Ignacio Amatt <ignacio_amatt@hotmail.com>
2021-09-13 00:22:13 -0300

    mas css

---
+++ src/components/UserPage/modules/TopContainerStyle.js
@@ -0,0 +1,13 @@
+import UserDetails from '../../../assets/USAF_photographer.jpg'
+
+export const banner = {
+  backgroundImage: 'url(' + UserDetails + ')',
+  //   borderStyle: 'solid',
+  height: '30vh',
+  display: 'flex',
+  alignItems: 'end',
+  backgroundRepeat: 'no-repeat',
+  backgroundSize: 'cover',
+  backgroundPosition: '0% 28%'
+
+}
