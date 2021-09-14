ab536a7151f6099df8680147e7dc148282228b8c
Ignacio Amatt <ignacio_amatt@hotmail.com>
2021-09-14 14:22:25 -0300

    avances? en cart

--- src/components/Cart/Cart.view.jsx
+++ src/components/Cart/Cart.view.jsx
@@ -1,17 +1,70 @@
+import { Container, Col, Row, InputGroup, Button, FormControl } from 'react-bootstrap'
+import { FormatedPrice } from '../../components/common/formatedPrice'
 import styles from './Cart.module.css'
 
-export const CartView = () => {
+export const CartView = ({ cart, total }) => {
   return (
     <>
       <div id={styles.banner} />
+
       <div id={styles.mainContainer}>
+
         <div id={styles.header}>
           <ul>
             <li className={styles.headerItem}> Carrito </li>
             <li className={styles.headerItem}> Guardados </li>
           </ul>
         </div>
-        body
+
+        <Container id={styles.body}>
+          {cart.map(product => (
+            <Row className={styles.productContainer} key={product.id}>
+              <Col lg={2}>
+                <img src={product.img} alt='' style={{ width: '80px' }} />
+              </Col>
+
+              <Col lg={6}>
+                <Row>
+                  {product.model}
+                </Row>
+                <Row>
+                  Marca: {product.brand}
+                </Row>
+
+                <Row>
+                  <Col>Eliminar</Col>
+                  <Col>Guardar para después</Col>
+
+                </Row>
+
+              </Col>
+
+              <Col lg={2}>
+                <InputGroup className='mb-3 w-50'>
+                  <Button variant='outline-secondary' id='button-addon1'>
+                    -
+                  </Button>
+                  <FormControl
+                    aria-label='Example text with button addon'
+                    aria-describedby='basic-addon1'
+                    value={product.qty}
+                  />
+                  <Button variant='outline-secondary' id='button-addon1'>
+                    +
+                  </Button>
+                </InputGroup>
+              </Col>
+
+              <Col lg={2}>
+                {FormatedPrice(product)}
+              </Col>
+            </Row>
+          )
+          )}
+          <Row>
+            Total: {FormatedPrice({ price: total })}
+          </Row>
+        </Container>
       </div>
     </>
   )
