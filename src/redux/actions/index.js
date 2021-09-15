f2ea9797f389e59c88cdef9aac932536fe12f722
erielilara <erieli.larad@gmail.com>
2021-09-15 15:42:22 -0500

    productActions file created :)

--- src/redux/actions/index.js
+++ src/redux/actions/index.js
@@ -1,12 +1,6 @@
 import axios from 'axios'
 import {
-  GET_PRODUCTS,
-  GET_PRODUCT_BY_ID,
-  DELETE_DETAILS,
-  GET_CATEGORIES,
   GET_REVIEWS,
-  SET_SEARCH_RESULT,
-  SET_SEARCH_STRING,
   ADD_PRODUCT_TO_CART,
   REMOVE_PRODUCT_FROM_CART,
   ADD_PRODUCT_TO_SAVED,
@@ -19,63 +13,10 @@ import {
 
 import { ApiURL } from '../../config/config'
 
-export const getProducts = () => {
-  return async function (dispatch) {
-    const response1 = await axios.get(`${ApiURL}/products`)
-    return dispatch({ type: GET_PRODUCTS, payload: response1.data })
-  }
-}
-
-export const getProductById = (id) => {
-  return async (dispatch) => {
-    const response = await axios.get(`${ApiURL}/products/detail/${id}`)
-    // const product = {
-    //   id: response.data.id,
-    //   brand: response.data.brand,
-    //   model: response.data.model,
-    //   img: response.data.img,
-    //   description: response.data.description,
-    //   price:
-    //     '$' +
-    //     new Intl.NumberFormat('es-ES').format(response.data.price.toString()),
-    //   points: response.data.points,
-    //   subCategoryId: response.data.subCategoryId
-    // }
-    return dispatch({ type: GET_PRODUCT_BY_ID, payload: response.data })
-  }
-}
-
-export const deleteDetails = () => {
-  return {
-    type: DELETE_DETAILS
-  }
-}
-
-export const getCategories = (payload) => {
-  return {
-    type: GET_CATEGORIES,
-    payload
-  }
-}
-
 export const getReviews = () => {
   return async function (dispatch) {
     const reviews = await axios.get(`${ApiURL}/products/reviews`)
     return dispatch({ type: GET_REVIEWS, payload: reviews.data })
-  }
-}
-
-export const setSearchResult = (searchResults) => {
-  return {
-    type: SET_SEARCH_RESULT,
-    payload: searchResults
-  }
-}
-
-export const setSearchString = (searchString) => {
-  return {
-    type: SET_SEARCH_STRING,
-    payload: searchString
   }
 }
 
