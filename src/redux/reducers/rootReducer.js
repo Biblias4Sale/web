b22a051f7588917c95ec3c8df996925b63c4a5d8
Ignacio Amatt <ignacio_amatt@hotmail.com>
2021-09-14 21:21:02 -0300

    Update rootReducer.js

--- src/redux/reducers/rootReducer.js
+++ src/redux/reducers/rootReducer.js
@@ -93,7 +93,8 @@ const rootReducer = (state = initialState, action) => {
         }
       } else {
         return { ...state, cart: { ...state.cart, saved: state.cart.saved.concat(action.payload) } }
-        
+      }
+
     case SUBTRACT_QTY_FROM_CART:
       state.cart.main.map(product => {
         if (product.id === action.payload) {
