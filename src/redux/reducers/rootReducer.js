1ff2dd07c0d90ed1d535813f79fbcb3d18678d4a
Ignacio Amatt <ignacio_amatt@hotmail.com>
2021-09-16 00:27:28 -0300

    Update rootReducer.js

--- src/redux/reducers/rootReducer.js
+++ src/redux/reducers/rootReducer.js
@@ -125,19 +125,9 @@ const rootReducer = (state = initialState, action) => {
       })
       return { ...state, cart: { ...state.cart, saved: state.cart.saved } }
 
-// case QTY_CHOOSE_FROM_CART:
-//   state.cart.main.forEach(product => {
-//     if (product.id === action.payload.id) {
-//       if (product.qty > 1) {
-//         product.qty = action.payload.qty
-//       }
-//     }
-//   })
-//   return { ...state, cart: { ...state.cart, saved: state.cart.main } }
-
-default:
-  return state
-}
+    default:
+      return state
+  }
 }
 
 export default rootReducer
