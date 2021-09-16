0573086c058c5656a3c4e7e1c464ef2c5f0e046d
erielilara <erieli.larad@gmail.com>
2021-09-15 20:28:18 -0500

    LikeIcon renderizado en la navbar cuando el usuario está logeado

--- src/components/NavBar/modules/IconsBox/index.js
+++ src/components/NavBar/modules/IconsBox/index.js
@@ -3,11 +3,15 @@ import { CartIcon } from './cartIcon'
 import { UserIcon } from './userIcon'
 import { SetSurface } from '../../NavBarStyle'
 import { LikeIcon } from './LikeIcon'
+import { useSelector } from 'react-redux'
 
 export const IconsBox = () => {
+  const logged = useSelector((state) => state.logged)
   return (
     <Container className='d-flex justify-content-around align-items-center' style={SetSurface('40vh')}>
-      <LikeIcon />
+      {logged
+        ? <LikeIcon />
+        : null}
       <CartIcon />
       <UserIcon />
     </Container>
