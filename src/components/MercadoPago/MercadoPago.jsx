fcc52a08d4cd7395118f77b127b357f8d9143089
Ignacio Amatt <ignacio_amatt@hotmail.com>
2021-09-18 13:11:52 -0300

    Update MercadoPago.jsx

--- src/components/MercadoPago/MercadoPago.jsx
+++ src/components/MercadoPago/MercadoPago.jsx
@@ -1,61 +1,60 @@
-import { useSelector } from "react-redux"
+import { useSelector } from 'react-redux'
 import axios from 'axios'
-import { useEffect, useState } from "react"
-import {ApiURL} from '../../config/config'
+import { useEffect, useState } from 'react'
+import { ApiURL } from '../../config/config'
 import { Modal } from 'react-bootstrap'
 import { Loading } from '../common/spinner'
-import { CheckInfo } from "./CheckInfo/CheckInfo"
+import { CheckInfo } from './CheckInfo/CheckInfo'
 // import { Pay } from './modules/Pay'
 
 export const MercadoPago = (props) => {
-    const user = useSelector(state => state.logged.user)
-    // const userCart = useSelector(state => state.userCart)
+  const user = useSelector(state => state.logged.user)
+  // const userCart = useSelector(state => state.userCart)
 
-    const [actualView, setActualView] = useState('init')
+  const [actualView, setActualView] = useState('init')
 
-    const [url, setUrl] = useState('');
-   
-    const check = () => {
-        if(user.address || user.city || user.province || user.cp || user.phone) return setActualView('pay')
-        if(!user.address || !user.city || !user.province || !user.cp || !user.phone) return setActualView('check')
-    }
+  const [url, setUrl] = useState('')
 
+  const check = () => {
+    if (user.address || user.city || user.province || user.cp || user.phone) return setActualView('pay')
+    if (!user.address || !user.city || !user.province || !user.cp || !user.phone) return setActualView('check')
+  }
 
-    const payment = async () => {
-          const response = await axios.post(`${ApiURL}/api/v1/mercadopago`, 
-          //HARTCODEADO PORQUE NO SE QUE DATOS MANDAR DEL CARRITO (:
-            {
-                "currency_id": "ARS",
-                "description": "ndfkjgnrjl",
-                "title": "Una camara Noilan",
-                "unit_price": 20000,
-                "quantity": 2
-             }
-          , {withCredentials: true})
-          return response.data
-        }
+  const payment = async () => {
+    const response = await axios.post(`${ApiURL}/api/v1/mercadopago`,
+      // HARTCODEADO PORQUE NO SE QUE DATOS MANDAR DEL CARRITO (:
+      {
+        currency_id: 'ARS',
+        description: 'ndfkjgnrjl',
+        title: 'Una camara Noilan',
+        unit_price: 20000,
+        quantity: 2
+      }
+      , { withCredentials: true })
+    return response.data
+  }
 
-    useEffect( () => {
-        check()
-        payment().then(res => setUrl(res.url))
-    }, [])
+  useEffect(() => {
+    check()
+    payment().then(res => setUrl(res.url))
+  }, [])
 
-    return (
-     <div>
+  return (
+    <div>
       <Modal
         show={props.show}
         onHide={props.onHide}
         {...props}
         size='lg'
         centered
-        >
+      >
         <Modal.Header>
           <button type='button' className='btn-close' aria-label='Close' onClick={props.onHide} />
         </Modal.Header>
 
         <Modal.Body>
-        {actualView === 'check' ? <CheckInfo /> : <Loading />}
-        {actualView === 'pay' ? <iframe title='Finaliza tu compra' src={url} style={{width:'100%', height:'600px'}}/> : <Loading />} 
+          {actualView === 'check' ? <CheckInfo /> : <Loading />}
+          {actualView === 'pay' ? <iframe title='Finaliza tu compra' src={url} style={{ width: '100%', height: '600px' }} /> : <Loading />}
         </Modal.Body>
       </Modal>
     </div>
