11052681b9d963cdbc015c1a1e4479c2c20b92ff
Lina Gutierrez Arribas <mikaelagutierrezarribas@gmail.com>
2021-09-19 17:22:45 -0700

    ...

--- src/components/MercadoPago/MercadoPago.jsx
+++ src/components/MercadoPago/MercadoPago.jsx
@@ -13,34 +13,31 @@ export const MercadoPago = (props) => {
   const [actualView, setActualView] = useState('check')
   const [url, setUrl] = useState('')
 
-  // const check = () => {
-  //   if (user?.address || user?.city || user?.province || user?.cp || user?.phone) return setActualView('pay')
-  //   if (!user?.address || !user?.city || !user?.province || !user?.cp || !user?.phone) return setActualView('check')
-  // }
-
   const payment = async () => {
     const response = await axios.post(`${ApiURL}/api/v1/mercadopago`,
       // HARTCODEADO PORQUE NO SE QUE DATOS MANDAR DEL CARRITO (:
-      {
+      [{
         currency_id: 'ARS',
         description: 'ndfkjgnrjl',
         title: 'Una camara Noilan',
         unit_price: 20000,
         quantity: 2
-      }
+      },
+      {
+        currency_id: 'ARS',
+        description: 'ndfkjgnrjl',
+        title: 'Una camara Noilan',
+        unit_price: 88000,
+        quantity: 2
+      }]
       , { withCredentials: true })
+      console.log(response.data, 'holis')
     return response.data
   }
 
   useEffect(() => {
     payment().then(res => setUrl(res.url))
   }, [])
-
-  // useEffect(() => {
-  //   if (user?.address || user?.city || user?.province || user?.cp || user?.phone) return setActualView('pay')
-  //   if (!user?.address || !user?.city || !user?.province || !user?.cp || !user?.phone) return setActualView('check')
-  //   payment().then(res => setUrl(res.url))
-  // }, [user?.address, user?.city, user?.phone, user?.cp, user?.province])
 
   return (
     <div>
@@ -56,8 +53,8 @@ export const MercadoPago = (props) => {
         </Modal.Header>
 
         <Modal.Body>
-          {actualView === 'check' ? <CheckInfo setActualView={setActualView} /> : null}
-          {actualView === 'pay' ? <iframe title='Finaliza tu compra' src={url} style={{ width: '100%', height: '600px' }} /> : null}
+          {actualView === 'pay' ? <CheckInfo setActualView={setActualView} /> : null}
+          {actualView === 'check' ? <iframe title='Finaliza tu compra' src={url} style={{ width: '100%', height: '600px' }} /> : null}
         </Modal.Body>
       </Modal>
     </div>
