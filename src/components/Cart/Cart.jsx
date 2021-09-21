4672818aa44a0d612a66768a8f739474eb90e2d0
erielilara <erieli.larad@gmail.com>
2021-09-21 11:19:46 -0500

    .

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -20,6 +20,7 @@ export const Cart = () => {
   const logged = useSelector(state => state.logged)
   const userID = useSelector(state => state.logged ? state.logged.user.id : null)
   const cartID = useSelector(state => state.logged ? state.logged.cart.id : null)
+  console.log('SOY CARTID', logged)
   const mainList = useSelector((state) => state.logged ? state.userCart : state.cart.main)
   const savedList = useSelector((state) => state.logged ? state.userSaved : state.cart.saved)
   const [actualView, setActualView] = useState('main')
