3d50db350c8ab98ac58f5d4006e4a003ab1a0ca6
GotoxyDev <contact@gotoxy.dev>
2021-09-22 01:13:54 -0300

    auth-google -working

--- src/RoutesWeb/index.js
+++ src/RoutesWeb/index.js
@@ -6,16 +6,20 @@ import ProductDetails from '../components/ProductDetails/ProductDetails'
 import UserPage from '../components/UserPage/UserPage'
 import NotFound from '../components/Error/Error404'
 import Cart from '../components/Cart/Cart'
-import LogOut from '../components/Session/LogOut/LogOut'
+import { Logout } from '../components/Session/LogOut/LogOut'
+import SuccessLogin from '../components/Session/modules/Login/Google/SuccessLogin'
+import FailureLogin from '../components/Session/modules/Login/Google/FailureLogin'
 
 export const RouterWeb = () => {
   const logged = useSelector(state => state.logged)
   return (
     <Switch>
       <Route exact path='/' component={Home} />
-      <Route exact path='/logout' component={LogOut} />
+      <Route exact path='/logout' component={Logout} />
       <Route exact path='/catalogo' component={Catalogue} />
       <Route exact path='/product/details/:id' component={ProductDetails} />
+      <Route exact path='/login/success' component={SuccessLogin} />
+      <Route exact path='/login/failure' component={FailureLogin} />
       <Route exact path='/cart' component={Cart} />
       {logged
         ? <Route exact path='/micuenta' component={UserPage} />
---
+++ src/components/Session/modules/Login/Google/FailureLogin.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const FailureLogin = () => {
+  return (
+    <div>login failed</div>
+  )
+}
+
+export default FailureLogin
