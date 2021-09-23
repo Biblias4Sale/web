4ed1adc181c6cd94e92aa4f4ca587392ea2a65ad
erielilara <erieli.larad@gmail.com>
2021-09-23 17:04:16 -0500

    boton favoritos en detalle de producto

--- src/components/ProductDetails/modules/productContainer.js
+++ src/components/ProductDetails/modules/productContainer.js
@@ -2,11 +2,19 @@ import { Container, Row } from 'react-bootstrap'
 import { ImgCol } from './imgCol'
 import { DetailsCol } from './detailsCol'
 import { ProductContainerStyle } from '../ProductDetailsStyle'
+import { ButtonLike } from '../../common/ButtonLike'
+import { useSelector } from 'react-redux'
 
 export const ProductContainer = ({ product }) => {
+  const logged = useSelector(state => state.logged)
   return (
     <Container style={ProductContainerStyle}>
       <Row>
+        {logged
+          ? (
+            <ButtonLike product={product} />
+            )
+          : null}
         <ImgCol img={product.img} />
         <DetailsCol product={product} />
       </Row>
