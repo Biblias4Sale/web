b22c456bed36b691429513a110b437e274130e64
Ignacio Amatt <ignacio_amatt@hotmail.com>
2021-09-24 02:29:32 -0300

    Console logs eliminados

--- src/components/Catalogue/modules/main/filterByPoints.js
+++ src/components/Catalogue/modules/main/filterByPoints.js
@@ -1,9 +1,7 @@
 export const filterByPoints = (options, filtredByPrice) => {
-  console.log('options RATING:', options.raiting)
   if (options.raiting.length === 0) return filtredByPrice
   else {
     return filtredByPrice.filter(product => {
-      console.log('PRODUCT RATING:', product.rating)
       return options.raiting.includes(product.rating)
     })
   }
