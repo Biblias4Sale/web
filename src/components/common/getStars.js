ea02971f1b2dee3c7887e0af9dd71294bf12e5ce
Ignacio Amatt <ignacio_amatt@hotmail.com>
2021-09-23 22:39:41 -0300

    Product Detail ya muestra promedio de rating

--- src/components/common/getStars.js
+++ src/components/common/getStars.js
@@ -1,10 +1,10 @@
 import { BsStarFill } from 'react-icons/bs'
 
 export const GetStarts = (product) => {
-  const points = product.points
+  const rating = product.rating
   const stars = []
 
-  for (let i = 0; i < points; i++) {
+  for (let i = 0; i < rating; i++) {
     stars.push(<BsStarFill key={i} />)
   }
 
