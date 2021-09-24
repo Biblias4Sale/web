2b6d161fd2f3f23a4c48ac7797d9098211fca4e6
Ignacio Amatt <ignacio_amatt@hotmail.com>
2021-09-24 03:12:43 -0300

    Mis compras muestra los carritos ordenados por fecha y con su estatus con colores

--- src/components/UserPage/Options/MyShopping/MyShoppingView.jsx
+++ src/components/UserPage/Options/MyShopping/MyShoppingView.jsx
@@ -5,14 +5,26 @@ import { Link } from 'react-router-dom'
 import moment from 'moment'
 
 export const MyShoppingView = ({ orders, makeReview }) => {
+  orders.sort(function (a, b) {
+    if (a.confirmationPending < b.confirmationPending) {
+      return 1
+    }
+    if (a.confirmationPending > b.confirmationPending) {
+      return -1
+    }
+    return 0
+  })
+
   return (
     <div>
       {orders.map((order, index) => {
         const formatedTotal = FormatedPrice({ price: order.totalAmount })
         return (
-          <Container key={index} style={{ border: 'solid 1px grey' }} className='m-2'>
-            <h4 className='d-flex text-success'>{order.status} </h4>
-            <h4>Total: {formatedTotal}</h4>
+          <Container key={index} style={{ border: 'solid 1px grey', padding: '1rem' }} className='m-2'>
+            {order.status === 'En preparación' ? <h4 className='d-flex text-primary'>{order.status} </h4> : null}
+            {order.status === 'Entregado' ? <h4 className='d-flex text-success'>{order.status} </h4> : null}
+            {order.status === 'Pendiente de confirmación de pago' ? <h4 className='d-flex text-warning'>{order.status} </h4> : null}
+            <h5>Total: {formatedTotal}</h5>
             <p className='d-flex'>{moment(order.confirmationPending).format('LLLL')}</p>
             <div>{order.productSolds?.map(product => {
               const formatedPrice = FormatedPrice(product)
@@ -28,7 +40,7 @@ export const MyShoppingView = ({ orders, makeReview }) => {
                     <Card.Body className='fw-bolder'>
                       <Row style={{ height: '10vh' }}><Card.Text className='text-center'>{product.brand + ' ' + product.model}</Card.Text></Row>
                       <Row><Card.Text className='d-flex justify-content-center'> {formatedPrice} </Card.Text></Row>
-                      {order.status === 'Entregado' ? <Row> <Button onClick={() => makeReview(product.idProductSold)}> Dejar review </Button> </Row> : null}
+                      {order.status === 'Entregado' ? <Row> <Button onClick={() => makeReview(product.idProductSold)}>Review</Button> </Row> : null}
                     </Card.Body>
                   </Card>
                 </div>
